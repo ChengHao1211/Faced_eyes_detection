5062b1e50e1a7b31c6010c5b434ee9adbe2f582f
ChengHao1211 <113260755+ChengHao1211@users.noreply.github.com>
2024-03-14 19:30:11 +0000

    upload files

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/ychen/CodingProgram/C++/opencvexample
+# Build directory: C:/Users/ychen/CodingProgram/C++/opencvexample/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
